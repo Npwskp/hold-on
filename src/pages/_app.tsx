356926974b8d4f520f2e193f7132d53d35cd6f1c
Npwskp <nopawit.sukolpuk@gmail.com>
2025-02-24 16:04:06 +0700

    Update Title and Icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>รอก่อนนะ</title>
+        <link rel="icon" href="/images/Main_Character.png" />
+        <meta name="description" content="รอก่อนนะ" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
