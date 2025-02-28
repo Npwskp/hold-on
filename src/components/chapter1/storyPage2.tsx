bcd47d94a15cd64a4f97c853f08a3df84397cabc
Npwskp <nopawit.sukolpuk@gmail.com>
2025-02-28 18:05:45 +0700

    Add story and bg image

--- src/components/chapter1/storyPage2.tsx
+++ src/components/chapter1/storyPage2.tsx
@@ -8,10 +8,19 @@ const mali = Mali({
 
 const StoryPage2 = () => {
   return (
-    <div className= {`w-full min-h-[100dvh] flex flex-col items-center justify-center ${mali.className} text-white text-lg md:text-2xl`}>
-        <p>ไอร้อนจากแสงแดด ปะทะใบหน้าของผม</p>
-        <p>ก่อนถูกชะล้างด้วยน้ำเย็นฉ่ำ</p>
-        <p>ที่อบอวนด้วยกลิ่นหอมจางๆ</p>
+    <div 
+      className={`w-full flex flex-col items-center justify-center ${mali.className} text-black text-lg md:text-2xl`}
+      style={{
+        backgroundImage: 'url(/images/chapter1/SongkranScene.png)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        minHeight: '100dvh',
+        position: 'relative',
+      }}
+    >
+      <div className='`flex flex-col justify-center items-center p-8 md:p-14 relative mb-[5vh] w-[85vw] md:w-[640px] aspect-[21/8] bg-white opacity-90 rounded-lg'>
+        <p className='text-center'>ไอร้อนจากแสงแดด ปะทะใบหน้าของผม ก่อนถูกชะล้างด้วยน้ำเย็นฉ่ำ ที่อบอวนด้วยกลิ่นหอมจางๆ</p>
+      </div>
     </div>
   )
 }
--- src/components/dialougeTemplate.tsx
+++ src/components/dialougeTemplate.tsx
@@ -14,13 +14,16 @@ const mali = Mali({
 const DialogueTemplate: React.FC<DialogueTemplateProps> = ({ storyData }) => {
   return (
     <div 
-      className="dialogue-container flex flex-col items-center justify-end"
+      className={`dialogue-container flex flex-col items-center justify-end w-screen md:w-[540px] mx-auto ${
+        storyData.backgroundImage?.includes('SongkranScene') ? 'md:w-screen' : ''
+      }`}
       style={{
-        backgroundImage: `url(${storyData.backgroundImage})`,
+        backgroundImage: `url(${storyData.backgroundImage || ''})`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         minHeight: '100vh',
-        position: 'relative'
+        position: 'relative',
+        aspectRatio: storyData.backgroundImage?.includes('SongkranScene') ? 'auto' : '16/9'
       }}
     >
       {storyData.characterImage && (
@@ -32,16 +35,19 @@ const DialogueTemplate: React.FC<DialogueTemplateProps> = ({ storyData }) => {
           className="w-[50vw] md:w-[25vw] h-auto me-28 md:me-40"
         />
       )}
-      <div className={`flex flex-col justify-center p-8 md:p-14 relative mb-[5vh] w-[85vw] md:w-[640px] aspect-[21/8] bg-white rounded-lg ${mali.className}`}>
-        {storyData.isCharacterDialogue && (
-          <div className="absolute -top-8 md:-top-10 left-[5%] bg-black w-[30%] md:w-[150px] aspect-[20/9] rounded-xl flex justify-center items-center">
-            <h2 className="text-xl md:text-2xl">{storyData.characterName}</h2>
+      
+      {storyData.text && (
+        <div className={`flex flex-col justify-center p-8 md:p-14 relative mb-[5vh] w-[85vw] md:w-[640px] aspect-[21/8] bg-white/90 rounded-lg ${mali.className}`}>
+          {storyData.isCharacterDialogue && (
+            <div className="absolute -top-8 md:-top-10 left-[5%] bg-black/90 w-[30%] md:w-[150px] aspect-[20/9] rounded-xl flex justify-center items-center">
+              <h2 className="text-xl md:text-2xl">{storyData.characterName}</h2>
+            </div>
+          )}
+          <div className="text-black text-lg md:text-xl">
+            {storyData.text}
           </div>
-        )}
-        <div className="text-black text-lg md:text-xl">
-          {storyData.text}
         </div>
-      </div>
+      )}
     </div>
   )
 }
